6867fa9865781fc44268258792e27a4c79115cdd
madhumithaaprasad <156235081+madhumithaaprasad@users.noreply.github.com>
2025-01-02 02:22:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener("scroll", () => {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor = "#e9b8c9"; // Darker pink when scrolled
-    } else {
-        navbar.style.backgroundColor = "#f8d7e8"; // Default light pink
-    }
-});
